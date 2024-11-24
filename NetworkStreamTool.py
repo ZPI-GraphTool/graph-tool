0db6a00fb642b780af5825a1f997107e4055c7f2
mikorzen <michal.korzen-tech@pm.me>
2024-11-24 23:30:01 +0100

    add threading to allow opening the web broswer simultaneously to running the app + pyinstaller shenanigans

--- NetworkStreamTool.py
+++ NetworkStreamTool.py
@@ -1,4 +1,10 @@
+import webbrowser
+from threading import Thread
+
 from app import shiny_app
 
 if __name__ == "__main__":
-    shiny_app.run()
+    run_shiny_app = Thread(target=shiny_app.run)
+    run_shiny_app.start()
+    webbrowser.open("http://localhost:8000")
+    run_shiny_app.join()
