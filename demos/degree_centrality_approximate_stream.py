0a26ca001538f4456c92f273bf559c660e87832f
zevli <95692593+zevli@users.noreply.github.com>
2024-12-17 21:50:34 +0100

    removed copy-paste dud

--- demos/degree_centrality_approximate_stream.py
+++ demos/degree_centrality_approximate_stream.py
@@ -4,7 +4,7 @@ from algorithms._config.interfaces import ResultList, StreamingAlgorithm
 class DegreeCentralityApproximateVersion(StreamingAlgorithm):
     def __init__(self) -> None:
         self.degrees = {}
-        self.results = {}
+
 
     def on_edge_calculate(self, edge: tuple) -> None:
         vertex_start = edge[0]
@@ -16,14 +16,9 @@ class DegreeCentralityApproximateVersion(StreamingAlgorithm):
         if vertex_end not in self.degrees:
             self.degrees[vertex_end] = 0
 
-        if vertex_start not in self.results:
-            self.results[vertex_start] = 0
-
-        if vertex_end not in self.results:
-            self.results[vertex_end] = 0
-
         self.degrees[vertex_start] = self.degrees[vertex_start] + 1
         self.degrees[vertex_end] = self.degrees[vertex_end] + 1
+
 
     def submit_results(self) -> ResultList:
         return list(self.degrees.items())
